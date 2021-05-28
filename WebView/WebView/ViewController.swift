f2ff0bfbeef85e88b088c21d476e8329425f442f
professordeng <2248262060@qq.com>
2021-05-28 17:43:08 +0800

    feat: load url

--- WebView/WebView/ViewController.swift
+++ WebView/WebView/ViewController.swift
@@ -10,9 +10,21 @@ import WebKit
 
 class ViewController: UIViewController {
 
+    let webView = WKWebView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.addSubview(webView)
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            webView.topAnchor.constraint(equalTo: view.topAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            webView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            webView.rightAnchor.constraint(equalTo: view.rightAnchor)
+        ])
+        if let url = URL(string: "https://www.baidu.com") {
+            webView.load(URLRequest(url: url))
+        }
     }
 }
 
